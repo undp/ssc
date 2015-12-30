8cc03c669d21f4c9b1fe3ba15488d47052917bde
Jonathan Smith <jonathan@peoplesized.com>
2015-12-30 23:15:10 +0000

    more script

--- admin_scripts/remove_11.js
+++ admin_scripts/remove_11.js
@@ -6,6 +6,7 @@ var fs = require('fs'),
     _ = require('lodash');
 
 ROOT = '../_ssc_projects/';
+DEFAULT_TEXT = "\n\n\n## A New Post\n\nEnter text in [Markdown](http://daringfireball.net/projects/markdown/). Use the toolbar above, or click the **?** button for formatting help.\n'"
 
 function project_files(){
   return _.reject(fs.readdirSync(ROOT), function(entry){
@@ -15,8 +16,8 @@ function project_files(){
 
 function broken_files(){
   return _.select(project_files(), function(file){
-    var keys = _.keys(project_data_from(file).attributes);
-    return _.includes(keys, "11");
+    var data = project_data_from(file);
+    return has_eleven(data.attributes) && default_content(data.body);
   })
 }
 
@@ -26,6 +27,46 @@ function project_data_from(file){
   return fm(data);
 }
 
+function fixed_content(file){
+  // Only receiving projects with '11' and unchanged default body content
+  var data = project_data_from(file);
+  data.body = data.attributes['11'];
+  delete data.attributes['11'];
+  return data;
+}
+
+function default_content(body) {
+  return true
+  return body === DEFAULT_TEXT;
+}
+
+function has_eleven(attributes) {
+  var keys = _.keys(attributes);
+  return _.includes(keys, "11");
+}
+
+function create_text_file(data){
+  var output = "---\n";
+
+}
+
+function stringify_attributes(attributes){
+  var string = "";
+  return _.each(attributes, function (key, value) {
+  })
+}
+
+function process_all(){
+  // Get all broken files
+  // Check 
+}
+
+
+
 // console.log(project_data_from(broken_files()[0]));
 // console.log(project_files().length)
-console.log(broken_files().length)
+// console.log(broken_files().length)
+console.log(fixed_content('xxxc7fbab17.txt'))
+content = fixed_content('xxxc7fbab17.txt');
+// console.log(project_data_from('xxxc7fbab17.txt'))
+fs.writeFileSync('test_output.txt', JSON.stringify(content));
